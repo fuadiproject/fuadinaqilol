01104c4977152f002c41bcd0854d96ec47719404
m.fuadi <user@Hols-MacBook-Pro.local>
2025-09-18 18:04:58 +0700

    feat: add database logging

--- index-ws.js
+++ index-ws.js
@@ -27,6 +27,11 @@ wss.on("connection", function connection(ws) {
     ws.send("Welcome to the server!");
   }
 
+  db.run(`
+    INSERT INTO visitors (count, time)
+    VALUES (${numClients}, datetime('now'))
+  `);
+
   ws.on("close", function close() {
     console.log("Client disconnected");
     const numClients = wss.clients.size;
@@ -48,4 +53,43 @@ wss.on("error", function error(error) {
 
 wss.on("close", function close() {
   console.log("WebSocket closed");
+});
+/** End websocket */
+
+/** Begin Database */
+const sqlite3 = require("sqlite3");
+const db = new sqlite3.Database(":memory:");
+
+db.serialize(function () {
+  db.run(
+    `CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+    )`
+  );
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", function (err, row) {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log("Shutting down DB");
+  db.close();
+}
+
+/** End Database */
+
+process.on("SIGINT", () => {
+  console.log("SIGINT received");
+  wss.clients.forEach(function each(client) {
+    client.close();
+  });
+  server.close(() => {
+    console.log("Server closed");
+    shutdownDB();
+  });
 });
